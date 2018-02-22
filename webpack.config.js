327f199adb84c95bb03b074a6ecf77a0296de774
LaroYeh <rola1989@yahoo.com.tw>
2018-02-22 17:35:01 +0800

    test

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: './dist/',
     filename: 'build.js'
   },
   module: {
